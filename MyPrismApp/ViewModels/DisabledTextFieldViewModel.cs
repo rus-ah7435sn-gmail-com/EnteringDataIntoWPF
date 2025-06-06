f25ffa74cfd6b9d1e5c204b91b89176839a9a462
r.ilchenko <r.ilchenko@web.de>
2025-06-06 12:28:55 +0200

    !

--- MyPrismApp/ViewModels/DisabledTextFieldViewModel.cs
+++ MyPrismApp/ViewModels/DisabledTextFieldViewModel.cs
@@ -8,14 +8,11 @@ namespace MyPrismApp.ViewModels
         public string TextValue
         {
             get { return _textValue; }
-            // Сеттер делаем приватным или защищенным, если изменение только через AppendText
-            // Для простоты примера оставим публичным, но учтем, что Mode=OneWay в View
             set { SetProperty(ref _textValue, value); }
         }
 
         public DisabledTextFieldViewModel()
         {
-            // TextValue уже инициализирован string.Empty при объявлении
         }
 
         public void AppendText(string text)
